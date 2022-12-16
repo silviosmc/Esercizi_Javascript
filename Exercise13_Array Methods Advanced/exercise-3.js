2a813c01260258b535880adff4b08551f46bab70
Silvio_Craparotta <75319688+silviosmc@users.noreply.github.com>
2022-12-16 04:29:24 +0100

    Add files via upload

---
+++ Exercise13_Array Methods Advanced/exercise-3.js
@@ -0,0 +1,24 @@
+function calculateAverageAge(persons) {
+  let ageToNum = persons.map((el) => el.age);
+  let sum = ageToNum.reduce((el1, el2) => el1 + el2);
+  let average = sum/persons.length;
+  return Math.round(average)
+  
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
