15a50a722c219ccad71de24ba78717a253c0636b
Silvio_Craparotta <75319688+silviosmc@users.noreply.github.com>
2023-01-05 01:33:06 +0100

    Add files via upload

---
+++ Exercise24_Destructuring/exercise-5.js
@@ -0,0 +1,12 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+// const { id, name, surname, old } = person;
+// console.log(id, name, surname, old);
+
+const { id, firstName, lastName, age } = person;
+console.log(id, firstName, lastName, age);
