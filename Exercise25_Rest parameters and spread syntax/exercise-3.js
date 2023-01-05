7e5f76e578f4e8a664ab2ec1f162a17820ffe321
Silvio_Craparotta <75319688+silviosmc@users.noreply.github.com>
2023-01-05 03:18:46 +0100

    Add files via upload

---
+++ Exercise25_Rest parameters and spread syntax/exercise-3.js
@@ -0,0 +1,8 @@
+let numberStore = [0, 1, 2];
+// let newNumber = 3;
+// numberStore.push(newNumber);
+// console.log(numberStore);
+
+let numberStorePlus = [...numberStore, 3];
+
+console.log(numberStorePlus);
