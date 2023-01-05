a85749bb7e581224a9e6d99a11444a51b37f3ce0
Silvio_Craparotta <75319688+silviosmc@users.noreply.github.com>
2023-01-05 03:15:59 +0100

    Add files via upload

---
+++ Exercise20_instanceof Operator/exercise-6.js
@@ -0,0 +1,38 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(obj){
+    if (obj instanceof Square){
+      return obj.side ** 2;
+    } else if (obj instanceof Rectangle){
+      return obj.width * obj.height;
+    } else if (obj instanceof Circle){
+      return Math.PI * obj.radius ** 2;
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
