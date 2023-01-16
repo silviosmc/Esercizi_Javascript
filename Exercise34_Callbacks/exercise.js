6fd51a8c0760c7d97f26198a8250a727a9ecdfe2
Silvio_Craparotta <75319688+silviosmc@users.noreply.github.com>
2023-01-16 03:11:34 +0100

    Add files via upload

---
+++ Exercise34_Callbacks/exercise.js
@@ -0,0 +1,7 @@
+
+function printAsyncName (callback, name) {
+    setTimeout(callback, 1000);
+    setTimeout(() => console.log(name), 2000);
+}
+
+printAsyncName(() => console.log("Hello"), "Silvio");
